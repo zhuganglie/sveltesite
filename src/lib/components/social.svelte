f83fffdf5903fd171d20c733826317b168e8f8f0
zhuganglie <iwifer@gmail.com>
2022-05-11 22:50:59 +0800

    update

--- src/lib/components/social.svelte
+++ src/lib/components/social.svelte
@@ -1,5 +1,5 @@
 <div class="flex items-center justify-center gap-3 my-4 text-2xl">
-   <a href="https://github.com/zhuganglie"><div class="i-tabler-brand-github" /></a>
-    <a href="https://twitter.com/zhugangliet"><div class="i-tabler-brand-twitter" /></a>
-    <a href="mailto:pyrrhonianpig@gmail.com"><div class="i-tabler-mail" /></a>
+   <a href="https://github.com/zhuganglie" target="_blank"><div class="i-tabler-brand-github" /></a>
+    <a href="https://twitter.com/zhugangliet" target="_blank"><div class="i-tabler-brand-twitter" /></a>
+    <a href="mailto:pyrrhonianpig@gmail.com" ><div class="i-tabler-mail" /></a>
 </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -18,10 +18,10 @@
 <h1 class="text-3xl uppercase">Welcome to my website</h1>
 <hr />
 <div class="md:flex items-start justify-center gap-8">
-    <center><img src="/images/notion-avatar.svg" alt="pic" class="w-1/3 h-auto md:w-48 md:h-48"/></center>
+    <center><img src="/images/notion-avatar.svg" alt="pic" class="w-1/3 h-auto md:w-42 md:h-42"/></center>
 	<div>
-	<p class="md:text-4xl font-bold">Hey everyone &#129306;</p>
-	<p class="md:text-5xl font-bold">I am Πύρρων Ρουάν</p>
+	<p class="text-xl md:text-4xl font-bold">Hey everyone &#129306;</p>
+	<p class="text-2xl md:text-5xl font-bold">I am Πύρρων Ρουάν</p>
 {#if start}
 	<p in:fly={{ delay: 1000, duration: 1000, easing: quintOut, x: -200 }} class="w-full md:w-2/3 leading-10 text-md">
 		I am a learning designer & facilitator based in Singapore. I am passionate about imparting knowledge and developing people.
