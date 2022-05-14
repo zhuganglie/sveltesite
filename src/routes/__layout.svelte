9ff42a5c781c60645128d30d821354e742f5b29a
zhuganglie <iwifer@gmail.com>
2022-05-14 22:54:03 +0800

    update

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -66,7 +66,7 @@ export let currentRoute: any
 </main>
 {/key}
 
-<aside class="md:flex md:flex-col md:items-center md:justify-center text-center mx-auto px-6 hidden md:w-1/5">
+<aside class="lg:flex lg:flex-col lg:items-center lg:justify-center text-center mx-auto px-6 hidden md:w-1/5">
     <div class="mb-4 font-bold text-xl">Contact Me</div>
     <Contact />
 </aside>
